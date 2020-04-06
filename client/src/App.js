dcd1599e394fde26b51921a3f118e418f056e87c
Tim Shaker <tim@tmshkr.com>
2020-04-06 12:39:23 -0700

    display users list

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./components/Users";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <Router>
+      <main className="App">
+        <Switch>
+          <Route path="/users" component={Users} />
+          <Redirect to="/users" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
